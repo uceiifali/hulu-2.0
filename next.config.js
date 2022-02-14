eaceabdba8bc17fa598a605e4b2308285685932f
uceiifali <musiicxxx@gmail.com>
2022-02-14 18:47:12 +0200

    third commit

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
